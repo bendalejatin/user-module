4f46483c29808a8e307e125a18bfdcf4a987d473
Hitendra213 <22amtics059@gmail.com>
2025-06-29 13:24:41 +0530

    some changes

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -13,8 +13,8 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import profile from "../Assets/user.png";
 import "./Dashboard.css";
 
-const BASE_URL = "http://localhost:5000"; // Adjust this to your backend URL
-//const BASE_URL = "https://dec-entrykart-backend.onrender.com" ; // deployment url
+// const BASE_URL = "http://localhost:5000"; // Adjust this to your backend URL
+const BASE_URL = "https://dec-entrykart-backend.onrender.com" ; // deployment url
 
 const Dashboard = () => {
   const [specificBroadcasts, setSpecificBroadcasts] = useState([]);
